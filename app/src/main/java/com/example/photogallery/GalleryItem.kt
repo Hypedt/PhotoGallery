25a088c5145ef6225831cb84e7ef9fe241f7643d
Tai Doan <26643555+Hypedt@users.noreply.github.com>
2021-03-10 20:15:03 -0800

    Initial commit

---
+++ app/src/main/java/com/example/photogallery/GalleryItem.kt
@@ -0,0 +1,8 @@
+package com.example.photogallery
+
+data class GalleryItem(
+    var title: String = "",
+    var id: String = "",
+    var url: String = ""
+)
+
---
+++ app/src/main/java/com/example/photogallery/api/FlickrApi.kt
@@ -0,0 +1,16 @@
+package com.example.photogallery.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=43eb8bf30be017abc1363c1b287e0817" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    fun fetchPhotos(): Call<String>
+}
